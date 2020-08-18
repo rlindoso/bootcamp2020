9b7992917f8760778bcd3555c9b7110baa69e8a3
Rafael Araújo <rafael.araujo@insole.com.br>
2020-08-18 20:57:10 -0300

    Importando CSS e imagens

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 
+import './App.css';
+import backgroundImage from './assets/background.jpg';
+
 import Header from './components/Header';
 
 function App() {
@@ -13,6 +16,9 @@ function App() {
 
     return (<>
         <Header title="Homepage" />
+
+        <img width={600} src={backgroundImage} />
+
         <ul>
             {projects.map(project => <li key={project}>{project}</li>)}
         </ul>
