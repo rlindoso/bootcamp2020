1260eea331dff0a5fd40cb52c84f1eef5c3362f7
Rafael Araújo <rafael.araujo@insole.com.br>
2020-08-26 22:04:57 -0300

    Debugando NodeJS

--- primeiro-projeto-node/src/routes/index.ts
+++ primeiro-projeto-node/src/routes/index.ts
@@ -1,4 +1,9 @@
 // src/routes/index.ts
 import { Router } from 'express';
 const routes = Router();
+
+routes.get('/', (req, res) => {
+  return res.json({ message: 'Hello World' });
+});
+
 export default routes;
--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -4,9 +4,8 @@ import routes from './routes';
 
 const app = express();
 
-app.get('/', (req, res) => {
-  return res.json({ message: 'Hello World' });
-});
+app.use(express.json);
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('Server started on port 3333');
