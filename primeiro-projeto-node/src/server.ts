9868157edc0806305dd431ac1c0cf6e2fe416c2d
Rafael Araújo <rafael.araujo@insole.com.br>
2020-08-26 21:50:56 -0300

    Padrões Projeto - Eslint, Prettier e Editor Config

---
+++ primeiro-projeto-node/src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+import routes from './routes';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  return res.json({ message: 'Hello World' });
+});
+
+app.listen(3333, () => {
+  console.log('Server started on port 3333');
+});
